368cc4ebe0ad644d1eafa938abf688818b426955
Khalil Monastiri <khalil.monastiri@cgi.com>
2025-11-23 10:37:11 +0100

    Initial commit

---
+++ cypress/e2e/spec.cy.ts
@@ -0,0 +1,21 @@
+
+
+describe('Test de connexion', () => {
+  it('En tant que utilisateur je veux pouvoir me connecter à mon compte', () => {
+    
+    // vist the page
+    cy.visit(' http://localhost:4200');
+
+    // Taper le username
+    cy.get('[data-testId="username"]').type('charles');
+
+    // Taper le mot de passe
+    cy.get('[data-testId="password"]').type('123');
+
+    // Appuyer sur le boutton "se connecter"
+    cy.get('[data-testId="loginBtn"]').click();
+
+    cy.location('pathname').should('eq', '/produits')
+
+  })
+});
